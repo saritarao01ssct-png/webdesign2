903f88ff1034eefe0b036cca65b907e4e4869e48
saritarao01ssct-png <saritarao01ssct@gmail.com>
2026-09-22 17:20:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function  add(){
+    var num1,num2,add;
+    one=parseFloat(document.getElementById("one").value);
+    two=parseFloat(document.getElementById("two").value);
+    add=one+two;
+    document.getElementById("answer").value=add;
+}
